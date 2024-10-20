5a58432fd34fc9e4ea0322fcaa8c7c77c07abca0
Lebone Montoedi <montoedilebone@gmail.com>
2024-10-20 04:14:14 +0200

    Try

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/react-template1'>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 )
